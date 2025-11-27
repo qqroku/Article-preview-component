c5af30757dd8e5d34c8fd4dc0dccfebe47b03c09
Miles Magday <219670370+qqroku@users.noreply.github.com>
2025-11-27 22:06:39 +0800

    add an effect to close share-popup when click outside the share-popup

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const shareBtn = document.getElementById("share_btn");
 const sharePopup = document.getElementById("share_popup");
 
-shareBtn.addEventListener("click", () => {
+shareBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
   sharePopup.classList.toggle("article-card__share-popup--active");
+});
+
+document.addEventListener("click", (e) => {
+  const clickedOutside =
+    !sharePopup.contains(e.target) && !shareBtn.contains(e.target);
+
+  if (clickedOutside) {
+    sharePopup.classList.remove("article-card__share-popup--active");
+  }
 });
